a544080f5dfb03525d8a8c05e58e768b73fa1b41
EslamElnady0 <eslamelnady254@gmail.com>
2026-06-29 14:59:51 +0300

    Created Cart Module

---
+++ Modules/Cart/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Cart",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Cart",
+            targets: ["Cart"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../MarktekNetworking"),
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.10.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Cart",
+            dependencies: [
+                .product(name: "MarktekNetworking", package: "MarktekNetworking"),
+                .product(name: "Swinject", package: "Swinject"),
+            ]
+        ),
+        .testTarget(
+            name: "CartTests",
+            dependencies: ["Cart"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
