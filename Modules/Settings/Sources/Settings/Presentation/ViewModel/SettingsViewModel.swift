612963cd45b4ea2fb5344b13b42704d88bf36cf6
Esraa Ehab <esraaehab530@gmail.com>
2026-07-02 09:02:59 -0700

    finisehd settings viewModel

---
+++ Modules/Settings/Sources/Settings/Presentation/ViewModel/SettingsViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Esraa Ehab on 02/07/2026.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 14.0, *)
+public class SettingsViewModel: ObservableObject {
+    
+    @Published public var user: UserProfile
+    
+    @AppStorage("selectedCurrency") public var selectedCurrency: AppCurrency = .usd
+    @AppStorage("isDarkMode") public var isDarkMode: Bool = false
+    
+    public init() {
+        self.user = UserProfile(
+            name: "Julianna Rossi",
+            email: "julianna.rossi@gmail.com"
+        )
+    }
+    
+    public func signOut() {
+        print("User signed out. Token cleared.")
+    }
+    
+    public func convertPrice(priceInUSD: Double) -> String {
+        let converted = priceInUSD * selectedCurrency.exchangeRateFromUSD
+        let symbol = selectedCurrency.rawValue.components(separatedBy: " ").last ?? ""
+        return String(format: "%.2f %@", converted, symbol)
+    }
+}
